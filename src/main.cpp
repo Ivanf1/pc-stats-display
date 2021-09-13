8f7294963207d751ba2a9ae2b0a11b3e0225fc94
Ivan <capobianco.ivan@gmail.com>
2021-09-13 18:08:26 +0200

    refactor: drawArcComponent refactor: drawArcStats refactor: drawLineComponent refactor: drawLineStats
    refactor: displayStats
    
    fix: remove flickering on update values

--- src/main.cpp
+++ src/main.cpp
@@ -6,70 +6,119 @@
 #include "drawing/drawing.h"
 
 #define FONT_2_SIZE 16 // size of font #2 is 16 pixel
+#define TEXT_MARGINB 10
+#define DEFAULT_BG_COLOR TFT_BLACK
+
+#define ARC_STATS_HEIGHT 85
+#define LINE_STATS_HEIGHT 70
 
 TFT_eSPI tft = TFT_eSPI();
+StaticJsonDocument<1024> doc;
 
 void setup() {
   Serial.begin(115200);
 
   tft.init();
-  tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(DEFAULT_BG_COLOR);
+
+  tft.setTextColor(TFT_WHITE, DEFAULT_BG_COLOR);
 }
 
 // TODO: map arc color to sensor value (e.g. (0 - 100) -> (green - yellow - orange - red))
-void displayArcStat(TFT_eSPI &tft, uint16_t center_x, uint16_t min_y, uint16_t max_y, const char* label, float value, uint16_t arc_radius) {
-  uint16_t y_space = (int)((max_y - min_y) - FONT_2_SIZE - arc_radius);
-  // set text alignment middle center
-  tft.setTextDatum(MC_DATUM);
+void drawArcComponent(TFT_eSPI &tft, uint16_t center_x, uint16_t min_y, uint16_t max_y, const char* label, float value, uint16_t arc_radius) {
+  uint16_t y_space = (max_y - min_y) - FONT_2_SIZE - arc_radius;
 
   tft.drawString(label, center_x, min_y, 2);
-  tft.drawFloat(value, 1, center_x, min_y + y_space, 2);
+  char tx[10];
+  sprintf(tx, "  %.1f  ", value);
+  tft.drawString(tx, center_x, min_y + y_space, 2);
 
-  // // map the arc length to the sensor value
-  long arc_len = constrain(map(value, 0, 100, 0, 60), 0, 60);
+  // map the arc length to the value
+  long arc_len = constrain(map(value, 0, 100, 1, 60), 1, 60);
+  // When drawing the new arc, if the background has a different color than
+  // the default background color (i.e. something else was previously drawn) that
+  // color may still be visible. This problem could be resolved by drawing a
+  // full-size default-background-colored arc, and then drawing the new arc, 
+  // but this causes noticeable flickering.
+  // Here, after drawing the new arc with the required length, we then draw a
+  // default-background-colored arc from the new arc ends to the max_length.
+  // This gets rid of anything that was left on the background without flickering.
+  uint16_t end_angle = (270 + (arc_len * FILL_ARC_SEG)) % 360;
   fillArc(tft, center_x, max_y, 270, arc_len, arc_radius, arc_radius, 8, rainbow(0));
+  fillArc(tft, center_x, max_y, end_angle, 60 - arc_len, arc_radius, arc_radius, 8, DEFAULT_BG_COLOR);
 }
 
-// TODO: refactor this thing
-void displayHardwareStats(TFT_eSPI &tft, uint16_t max_x, uint16_t min_y, uint16_t max_y, const char* hw_name, const char* s1, const float s1_val, const char* s2, const float s2_val) {
+void drawLineComponent(TFT_eSPI &tft, uint16_t min_x, uint16_t max_x, uint16_t min_y, uint16_t max_y, const char* label, float value) {
+  uint16_t y_space = (max_y - min_y) - FONT_2_SIZE - 8;
+  uint16_t center_x = (max_x - min_x) / 2;
+
+  tft.drawString(label, center_x, min_y, 2);
+  char tx[10];
+  sprintf(tx, "  %.1f  ", value);
+  tft.drawString(tx, center_x, min_y + y_space, 2);
+
+  // map the line length to the value
+  long line_len = constrain(map(value, 0, 100, min_x, max_x), min_x, max_x);
+  // When drawing the new rectangle, if the background has a different color than
+  // the default background color (i.e. something else was previously drawn) that
+  // color may still be visible. This problem could be resolved by drawing a
+  // full-size default-background-colored rectangle, but this causes noticeable flickering.
+  // Here, after drawing the new line with the required length, we then draw a
+  // default-background-colored line from the new line ends to the max_x.
+  // This gets rid of anything that was left on the background without flickering.
+  tft.fillRect(min_x, max_y - 8, line_len, 8, rainbow(0));
+  tft.fillRect(line_len, max_y - 8, max_x, 8, DEFAULT_BG_COLOR);
+}
+
+void drawArcStats(TFT_eSPI &tft, uint16_t min_x, uint16_t max_x, uint16_t min_y, uint16_t max_y, const char* hw_name, const JsonArray &sensors) {
   uint16_t center_x = max_x / 2;
-  uint16_t left_center_x = center_x / 2;
-  uint16_t right_center_x = center_x + left_center_x;
-  
+  size_t num_sensors = sensors.size();
+  uint16_t column_space = max_x / num_sensors;
+  uint16_t sensor_center_x = column_space / num_sensors;
+
   tft.setTextDatum(MC_DATUM);
-  tft.drawString(hw_name, center_x, min_y + 10, 2);
 
-  // reset screen to black before re-drawing
-  // doing it separately causes less noticeable flickering
-  tft.fillRect(0, min_y + 26, center_x, max_y, TFT_BLACK);
-  displayArcStat(tft, left_center_x, min_y + 26, max_y, s1, s1_val, 26);
+  tft.drawString(hw_name, center_x, min_y + TEXT_MARGINB, 2);
 
-  tft.fillRect(center_x, min_y + 26, max_x, max_y, TFT_BLACK);
-  displayArcStat(tft, right_center_x, min_y + 26, max_y, s2, s2_val, 26);
+  for (uint8_t i = 0; i < num_sensors; i++) {
+    uint16_t center = (column_space * i) + sensor_center_x;
+    drawArcComponent(tft, center, min_y + TEXT_MARGINB + FONT_2_SIZE, max_y, sensors[i]["t"], sensors[i]["v"], 26);
+  }
 }
 
-// TODO: refactor this thing
+void drawLineStats(TFT_eSPI &tft, uint16_t min_x, uint16_t max_x, uint16_t min_y, uint16_t max_y, const char* hw_name, const JsonArray &sensors) {
+  uint16_t center_x = max_x / 2;
+  size_t num_sensors = sensors.size();
+
+  tft.setTextDatum(MC_DATUM);
+
+  tft.drawString(hw_name, center_x, min_y + TEXT_MARGINB, 2);
+
+  for (uint8_t i = 0; i < num_sensors; i++) {
+    drawLineComponent(tft, min_x, max_x, min_y + TEXT_MARGINB + FONT_2_SIZE, max_y, sensors[i]["t"], sensors[i]["v"]);
+  }
+}
+
 void displayStats(JsonDocument &doc, TFT_eSPI &tft) {
-  const char* hw_name = doc[0]["n"].as<const char*>();
-  const char* s1 = doc[0]["s"][0]["t"].as<const char*>();
-  float s1_val = doc[0]["s"][0]["v"].as<float>();
-  const char* s2 = doc[0]["s"][1]["t"].as<const char*>();
-  float s2_val = doc[0]["s"][1]["v"].as<float>();
+  size_t num_hw = doc.size();
+  uint16_t y_start = 0, y_max = 0;
 
-  displayHardwareStats(tft, 240, 0, 85, hw_name, s1, s1_val, s2, s2_val);
-
-  hw_name = doc[1]["n"].as<const char*>();
-  s1 = doc[1]["s"][0]["t"].as<const char*>();
-  s1_val = doc[1]["s"][0]["v"].as<float>();
-  s2 = doc[1]["s"][1]["t"].as<const char*>();
-  s2_val = doc[1]["s"][1]["v"].as<float>();
-
-  displayHardwareStats(tft, 240, 86, 171, hw_name, s1, s1_val, s2, s2_val);
+  for (int i = 0; i < num_hw; i++) {
+    y_start = y_max;
+    const char* hw_name = doc[i]["n"].as<const char*>();
+    int type = doc[i]["t"].as<int>();
+    if (type == 0) {
+      y_max += ARC_STATS_HEIGHT;
+      drawArcStats(tft, 0, 240, y_start, y_max, hw_name, doc[i]["s"]);
+    } else if (type == 1) {
+      y_max += LINE_STATS_HEIGHT;
+      drawLineStats(tft, 0, 240, y_start, y_max, hw_name, doc[i]["s"]);
+    }
+  }
 }
 
 void loop() {
   static uint8_t buffer[1024];
-  static StaticJsonDocument<1024> doc;
 
   if (Serial.available()) {
     Serial.readBytesUntil('\n', buffer, sizeof(buffer));
@@ -77,7 +126,7 @@ void loop() {
     if (error) {
       Serial.println(error.c_str());
     } else {
-      Serial.println("ok");
+      Serial.println(doc.memoryUsage());
       displayStats(doc, tft);
     }
   }
